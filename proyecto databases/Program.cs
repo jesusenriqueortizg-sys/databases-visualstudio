1221f883cd5a5893550a5e63ef4f9ffcc1cfaab2
jesusenriqueortizg-sys <jesusenriqueortizg@gmail.com>
2026-02-26 16:55:52 -0400

    2

--- proyecto databases/Program.cs
+++ proyecto databases/Program.cs
@@ -11,7 +11,7 @@ namespace proyecto_databases
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Menu());
         }
     }
 }
